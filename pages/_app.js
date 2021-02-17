1f697cb294218b240f9b9af1e3f3a33074609abb
Pankaj Patidar <pankaj9296@gmail.com>
2021-02-17 14:36:19 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,19 @@
 import { GeistProvider, CssBaseline } from '@geist-ui/react'
 
 function MyApp({ Component, pageProps }) {
+  
+  const myTheme = {
+      "type": "Custom",
+      "palette": {
+        "success": "#6C63EE", // primary/theme color
+        "link": "#6C63EE",
+        "successLight": "#7b73ff",
+        "successDark": "#574fdb",
+      }
+    }
+  
   return (
-    <GeistProvider>
+    <GeistProvider themes={[myTheme]} themeType="Custom">
       <CssBaseline />
       <Component {...pageProps} />
     </GeistProvider>
